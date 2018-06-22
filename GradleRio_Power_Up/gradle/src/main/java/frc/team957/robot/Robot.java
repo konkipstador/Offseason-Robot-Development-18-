57ec94ac2989b2375299ddb3f4fd53994c8cfb26
Caleb S <cns.scratch@gmail.com>
2018-06-22 11:07:37 -0700

    Pathfinder control loop implemented, ready to begin testing 6/22.

--- GradleRio_Power_Up/gradle/src/main/java/frc/team957/robot/Robot.java
+++ GradleRio_Power_Up/gradle/src/main/java/frc/team957/robot/Robot.java
@@ -17,7 +17,7 @@ import jaci.pathfinder.modifiers.TankModifier;
 
 public class Robot extends TimedRobot {
     
-    AHRS m_navx = new AHRS(I2C.Port.kMXP);  // The Nav-X attached to the MXP
+    AHRS _navx = new AHRS(I2C.Port.kMXP);  // The Nav-X attached to the MXP
 
     Joystick _dualshock4 = new Joystick(0);  // Dualshock 4 gamepad serving as drive controller
 
@@ -55,8 +55,8 @@ public class Robot extends TimedRobot {
     int _encoder_ticks_per_revolution = 512; // TestyBot uses 128 tick Quadrature encoders, so 128*4 ticks
 
     // Pathfinder EncoderFollower objects
-    EncoderFollower _left_encoder;
-    EncoderFollower _right_encoder;
+    EncoderFollower _left_encoder_follower;
+    EncoderFollower _right_encoder_follower;
 
     // EncoderFollower PID variables
     double _encoder_kp = 1; // Proportianal gain
@@ -70,14 +70,14 @@ public class Robot extends TimedRobot {
         _trajectory_modifier.modify(_wheelbase_width);  // Modify the trajectory based on the robot's width
 
         // Left Side
-        _left_encoder = new EncoderFollower(_trajectory_modifier.getLeftTrajectory());
-        _left_encoder.configureEncoder(0, _encoder_ticks_per_revolution, _wheel_size);
-        _left_encoder.configurePIDVA(_encoder_kp, 0, _encoder_kd, _encoder_kv, _encoder_ka);
+        _left_encoder_follower = new EncoderFollower(_trajectory_modifier.getLeftTrajectory());
+        _left_encoder_follower.configureEncoder(0, _encoder_ticks_per_revolution, _wheel_size);
+        _left_encoder_follower.configurePIDVA(_encoder_kp, 0, _encoder_kd, _encoder_kv, _encoder_ka);
 
         // Right Side
-        _right_encoder = new EncoderFollower(_trajectory_modifier.getRightTrajectory());
-        _right_encoder.configureEncoder(0, _encoder_ticks_per_revolution, _wheel_size);
-        _right_encoder.configurePIDVA(_encoder_kp, 0, _encoder_kd, _encoder_kv, _encoder_ka);
+        _right_encoder_follower = new EncoderFollower(_trajectory_modifier.getRightTrajectory());
+        _right_encoder_follower.configureEncoder(0, _encoder_ticks_per_revolution, _wheel_size);
+        _right_encoder_follower.configurePIDVA(_encoder_kp, 0, _encoder_kd, _encoder_kv, _encoder_ka);
         // ---END OF PATHFINDER SETUP--
 
 
@@ -96,7 +96,7 @@ public class Robot extends TimedRobot {
 
     public void autonomousInit() { 
         // Zero sensors
-        m_navx.reset();
+        _navx.reset();
         _talon_left_master.setSelectedSensorPosition(0, 0, _talon_timeout);
         _talon_right_master.setSelectedSensorPosition(0, 0, _talon_timeout);
     }
@@ -104,14 +104,36 @@ public class Robot extends TimedRobot {
     public void disabledPeriodic() { 
 
     }
-
+    
+    int _left_encoder_output = 0;
+    int _right_encoder_output = 0;
+    double _gyro_heading = 0;
+    double _left_motor_power = 0;
+    double _right_motor_power = 0;
+    double _desired_heading = 0;
+    double _turn_power = 0;
+    double _angle_offset = 0;
     public void autonomousPeriodic() { 
 
+        // Pathfinder control loop
+        _left_encoder_output = _talon_left_master.getSelectedSensorPosition(0);     // Pull encoder values
+        _right_encoder_output = _talon_right_master.getSelectedSensorPosition(0);
+        _gyro_heading = _navx.getAngle();                                           // Pull gyroscope angle
+
+        _left_motor_power = _left_encoder_follower.calculate(_left_encoder_output);
+        _right_motor_power = _right_encoder_follower.calculate(_right_encoder_output);  // Calculate motor power without turn adjustment
+
+        _desired_heading = Pathfinder.r2d(_left_encoder_follower.getHeading());     // Calculate necessary turn power
+        _angle_offset = Pathfinder.boundHalfDegrees(_desired_heading - _gyro_heading);
+        _turn_power = 0.8 * (-1/80) * _angle_offset;
+  
+        _talon_left_master.set(ControlMode.PercentOutput, _left_motor_power + _turn_power);     // Drive the robot
+        _talon_right_master.set(ControlMode.PercentOutput, _right_motor_power - _turn_power);
     }
 
     public void teleopPeriodic() { 
         // Tank Drive
-        _talon_left_master.set(ControlMode.PercentOutput, _dualshock4.getRawAxis(1));
+        _talon_left_master.set(ControlMode.PercentOutput, _dualshock4.getRawAxis(1));   
         _talon_right_master.set(ControlMode.PercentOutput, _dualshock4.getRawAxis(5));
     }
 
