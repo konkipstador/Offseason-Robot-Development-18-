f8b1a80952845f9f8e24eff2a07f1e55ee936848
konkipstador <shilling.frcrobotics@gmail.com>
2018-08-20 17:11:05 -0700

    Add files via upload
    Git commit via browser

---
+++ Motion Profiling Test/Robot_Rebuild/2018/Robot_Rebuild/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,180 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import java.util.ArrayList;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.SerialPort.Port;
+import frc.robot.AutoPaths.AdvWaypoint;
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Trajectory;
+import jaci.pathfinder.followers.EncoderFollower;
+import jaci.pathfinder.modifiers.TankModifier;
+
+
+public class Robot extends TimedRobot {
+
+    RobotDrive m_drivetrain = RobotDrive.getInstance();
+    Joystick m_dualshock = new Joystick(0);
+
+    ArrayList<AdvWaypoint> m_paths;
+    Trajectory.Config m_trajectoryConfig;
+    ArrayList<EncoderFollower> m_leftFollower = null;
+    ArrayList<EncoderFollower> m_rightFollower = null;   
+    int m_pathNumber = 0;
+    double m_timeInManeuver;
+    double m_totalTime = 0;
+
+
+    SerialPort m_serialPort = new SerialPort(57600, Port.kUSB1);
+
+    public void robotInit() {
+        m_drivetrain.setTalonIDs(0,1,2,3,4,5);  // 0-2 are left motors, 3-5 are right motors
+        m_drivetrain.setConversionValue((3.94*Math.PI)/512);  // Wheel circumfrence over encoder ticks; ~0.0242 inches per tick. 
+        m_drivetrain.invertRight(true);
+        m_drivetrain.setEncPhase(true, true);
+        
+        // Pathing method                    // Number of samples          // Time per loop, velocity, Accel, Jerk
+        m_trajectoryConfig = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_FAST, 0.02, 15, 15, 500);
+
+        // Generates the Switch auto path
+        m_paths = new ArrayList<AdvWaypoint>();
+        m_paths.add(AutoPaths.centerToRightSwitch);
+        m_paths.add(AutoPaths.rightSwitchToCenter);
+        m_paths.add(AutoPaths.centerToCube);
+
+        m_leftFollower = new ArrayList<EncoderFollower>();
+        m_rightFollower = new ArrayList<EncoderFollower>();
+        for (int i=0; i<m_paths.size(); i++) 
+        { 
+            m_leftFollower.add(new EncoderFollower((new TankModifier(Pathfinder.generate(m_paths.get(i).getWaypointArray(),m_trajectoryConfig)).modify(26)).getLeftTrajectory()));
+            m_leftFollower.get(i).configureEncoder(0,512,3.94);
+            m_leftFollower.get(i).configurePIDVA(0.05,0,0, 1/15,0);
+
+            m_rightFollower.add(new EncoderFollower((new TankModifier(Pathfinder.generate(m_paths.get(i).getWaypointArray(),m_trajectoryConfig)).modify(26)).getRightTrajectory()));
+            m_rightFollower.get(i).configureEncoder(0,512,3.94);
+            m_rightFollower.get(i).configurePIDVA(0.05,0,0, 1/15,0);
+        }
+    }
+
+    public void autonomousInit() {
+        m_pathNumber = 0;
+        m_timeInManeuver = 0;
+        m_totalTime = 0;
+        m_liftCommandSent = false;
+    }
+
+    public void disabledInit(){
+        for(int i=0; i<m_leftFollower.size(); i++){
+            m_leftFollower.get(i).reset();
+            m_rightFollower.get(i).reset();
+        }        
+        m_drivetrain.resetEncoders();
+    }
+
+    double leftOutput = 0;
+    double rightOutput = 0;
+    boolean m_liftCommandSent = false;
+
+    /**
+     * This is the control loop for Pathfinder. The control loop functions as such:
+     * 
+     * 1.   The loop checks if the delay value has passed. If it hasn't, it will raise the elevator if it is
+     *      programmed to be raised immediatly. The command to raise it will be sent once.
+     * 
+     * 2.   When the delay is up, it will update the left/right EncoderFollower object for the current maneuver.
+     *      
+     * 3.   The loop will then attempt to calculate how far the robot has strayed from it's path. It will then
+     *      initalize a variable with an offset value to keep the robot on the right track.
+     * 
+     * 4.   The robot will then set its velocity to the EncoderFollower's output plus or minus the turn
+     *      value. Steps 2-4 will repeat until the maneuver is complete
+     * 
+     * 5.   Once the maneuver is complete, it will stop the robot, reset the encoders, reset m_timeInManeuver,
+     *      and load the next maneuver if it exists.
+     * 
+     * The loop can also check when to move the elevator
+     */
+    //double m_turn
+    public void autonomousPeriodic() {
+
+        // Checks if the time spent delaying is equal to the delay value
+        if(m_timeInManeuver >= m_paths.get(m_pathNumber).m_delay && !m_leftFollower.get(m_pathNumber).isFinished()){
+            
+            // Flips the encoder input and final output if the path is set to be inverted
+            if(m_paths.get(m_pathNumber).isInverted()){
+                leftOutput = -m_leftFollower.get(m_pathNumber).calculate(-m_drivetrain.getLeftEnc());
+                rightOutput = -m_rightFollower.get(m_pathNumber).calculate(-m_drivetrain.getRightEnc());       
+            }else{
+                leftOutput = m_leftFollower.get(m_pathNumber).calculate(m_drivetrain.getLeftEnc());
+                rightOutput = m_rightFollower.get(m_pathNumber).calculate(m_drivetrain.getRightEnc());
+            }
+
+            double gyroHeading = m_drivetrain.getHeading();
+            double desiredHeading = Pathfinder.r2d(m_leftFollower.get(m_pathNumber).getHeading());
+            double angleDifference = Pathfinder.boundHalfDegrees(desiredHeading - gyroHeading);
+            double turn = 0.8 * (-1/80) * angleDifference;
+            
+            m_drivetrain.drive(leftOutput, rightOutput);
+            System.out.println(m_totalTime);
+
+            if(!m_liftCommandSent){
+                m_liftCommandSent = true;
+                // TODO Program lift code
+            }
+            
+        }else{
+            m_drivetrain.stop();
+
+            if(m_paths.get(m_pathNumber).shouldLiftRaiseBeforeDelay() && !m_liftCommandSent){
+                m_liftCommandSent = true;
+                // TODO Program lift code
+            }
+        }
+
+        // Checks if current path is complete
+        if(m_leftFollower.get(m_pathNumber).isFinished()){
+            m_drivetrain.stop();
+            m_drivetrain.resetEncoders();
+            m_timeInManeuver = 0;
+            if(m_pathNumber != m_paths.size()-1){
+                m_pathNumber++;
+            }           
+        }
+
+        // Increases the loop counter
+        m_timeInManeuver += 0.02;
+        m_totalTime += 0.02;
+    }
+
+    public void teleopPeriodic() {
+        m_drivetrain.drive(-m_dualshock.getRawAxis(1),-m_dualshock.getRawAxis(5));    // Tank drive
+    }
+
+    public void robotPeriodic() {
+
+        try{
+
+            String data = m_serialPort.readString();
+            if(data != ""){
+                System.out.println(data);
+            }
+            
+
+        }catch(Exception e){}
+
+    }
+
+    public void loadNextManeuver(){
+        
+    }
+
+}
---
+++ Motion Profiling Test/Robot_Rebuild/2018/Robot_Rebuild/src/main/java/frc/robot/RobotDrive.java
@@ -0,0 +1,135 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.I2C;
+
+public class RobotDrive{
+
+    private static RobotDrive m_robotDrive; // Static synchronized robot drive object
+
+    AHRS m_navX = new AHRS(I2C.Port.kMXP);  // NavX gyroscope
+    TalonSRX m_leftMotor1, m_leftMotor2, m_leftMotor3, m_rightMotor1, m_rightMotor2, m_rightMotor3;    // Drivetrain motor controllers
+    
+    private int m_leftTalonEnc = 0, m_rightTalonEnc = 0;        // Variables for storing raw encoder values
+    private double m_leftTalonDist = 0, m_rightTalonDist = 0;   // Variables for storing distance values derived from the encoder
+    private double m_conversionValue = 0;                       // Stores the value that converts from raw encoder output to the desired units
+    private double m_heading = 0;
+
+    private RobotDrive(){}  // Empty Constructor
+
+    // Creates the drivetrain object if it is not already created, then returns it
+    public static synchronized RobotDrive getInstance(){
+        if (m_robotDrive == null)
+            m_robotDrive = new RobotDrive();
+
+        return m_robotDrive;     
+    }
+
+    // Sets TalonSRX IDs
+    public void setTalonIDs(int left1, int left2, int left3, int right1, int right2, int right3){
+
+        // Left 1 will be the talon which the encoder will pull from
+        m_leftMotor1 = new TalonSRX(left1);
+        m_leftMotor2 = new TalonSRX(left2);
+        //m_leftMotor3 = new TalonSRX(left3);
+        m_leftMotor1.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 50);
+
+        // Right 1 will be the talon which the encoder will pull from
+        m_rightMotor1 = new TalonSRX(right1);
+        m_rightMotor2 = new TalonSRX(right2);
+       // m_rightMotor3 = new TalonSRX(right3);
+        m_rightMotor1.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 50);
+    }
+
+    // Sets both sides of the Robot to move at different motor powers
+    public void drive(double leftPower, double rightPower){
+        m_leftMotor1.set(ControlMode.PercentOutput, leftPower);
+        m_leftMotor2.set(ControlMode.PercentOutput, leftPower);
+        //m_leftMotor3.set(ControlMode.Position, leftPower);
+
+        m_rightMotor1.set(ControlMode.PercentOutput, rightPower);
+        m_rightMotor2.set(ControlMode.PercentOutput, rightPower);
+        //m_rightMotor3.set(ControlMode.Position, rightPower);
+    }
+
+    // Disables motor power
+    public void stop(){
+        m_leftMotor1.set(ControlMode.PercentOutput, 0);
+        m_leftMotor2.set(ControlMode.PercentOutput, 0);
+        //m_leftMotor3.set(ControlMode.Position, 0);
+
+        m_rightMotor1.set(ControlMode.PercentOutput, 0);
+        m_rightMotor2.set(ControlMode.PercentOutput, 0);
+        //m_rightMotor3.set(ControlMode.Position, 0);
+    }
+
+    public void invertRight(boolean value){
+        m_rightMotor1.setInverted(value);
+        m_rightMotor2.setInverted(value);
+        //m_rightMotor3.setInverted(true);
+    }
+
+    public void invertLeft(boolean value){
+        m_leftMotor1.setInverted(value);
+        m_leftMotor2.setInverted(value);
+        //m_leftMotor3.setInverted(true);
+    }
+
+    // Sets if phase should be shifted for each encoder
+    public void setEncPhase(boolean leftPhase, boolean rightPhase){
+        m_leftMotor1.setSensorPhase(leftPhase);
+        m_rightMotor1.setSensorPhase(rightPhase);
+    }
+
+    // Sets the left talon encoder value to it's respective variable and returns it
+    public int getLeftEnc(){
+        m_leftTalonEnc = m_leftMotor1.getSelectedSensorPosition(0);
+        return m_leftTalonEnc;
+    }
+
+    // Sets the right talon encoder value to it's respective variable and returns it
+    public int getRightEnc(){
+        m_rightTalonEnc = m_rightMotor1.getSelectedSensorPosition(0);
+        return m_rightTalonEnc;
+    }
+
+    // Sets the conversion value used to convert between raw encoder ticks to 
+    public void setConversionValue(double conversionValue){
+        m_conversionValue = conversionValue;
+    }
+
+    // Returns the distance the left side of the robot has traveled, calculated by multiplying the encoder value by the conversion value
+    public double getLeftDistance(){
+        m_leftTalonDist = getLeftEnc()*m_conversionValue;
+        return m_leftTalonDist;
+    }
+
+    // Returns the distance the right side of the robot has traveled, calculated by multiplying the encoder value by the conversion value
+    public double getRightDistance(){
+        m_rightTalonDist = getRightEnc()*m_conversionValue;
+        return m_rightTalonDist;
+    }
+
+    // Returns the NavX's calculated robot heading
+    public double getHeading(){
+        m_heading = m_navX.getAngle();
+        return m_heading;
+    }
+
+    public void resetEncoders(){
+        m_leftMotor1.setSelectedSensorPosition(0, 0, 50);
+        m_rightMotor1.setSelectedSensorPosition(0, 0, 50);
+    }
+
+    public double getLeftSpeed(){
+        return m_leftMotor1.getMotorOutputPercent();
+    }
+
+    public double getRightSpeed(){
+        return m_rightMotor1.getMotorOutputPercent();
+    }
+}
